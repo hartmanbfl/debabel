c3620c1fed0426b838d9f1a7685db1c0f8f26cd3
kemi.olowo <ooolowofoyeku@gmail.com>
2023-10-14 13:27:26 +0100

    update

--- pages/translate.js
+++ pages/translate.js
@@ -1,18 +1,22 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import styles from '../styles/Translate.module.css'
 import { io } from 'socket.io-client'
 import { useRouter } from 'next/router'
 let socket
 
 const Translate = () => {
-
+    const [translate, setTranslate] = useState()
+    const [transcript, setTranscript] = useState()
+    const [audio, setAudio] = useState(false)
     const router = useRouter()
     // const {push} = useRouter()
     const {serviceId, language} = router.query
 
     useEffect(() => {
+        if(router.isReady){
    socketInitializer()
-  }, [])
+        }
+  }, [router.isReady])
 
   const socketInitializer = () => {
     socket = io('https://live-pegasus-first.ngrok-free.app')
@@ -25,18 +29,52 @@ const Translate = () => {
     })
 
     const room = `${serviceId}:${language}`; 
+    const room2 = `${serviceId}:transcript`
+    console.log(room)
     socket.emit('join', room)
+    socket.emit('join', room2)
+
+    socket.on('transcript', (msg) => {
+        console.log(msg)
+        setTranscript(msg)
+    })
+
+    socket.on('translation', (msg) => {
+        console.log(msg)
+        setTranslate(msg)
+    })
   }
 
   useEffect(() => {
-    
-  }, [])
+    const addTranslate = () => {
+        const div = document.getElementById('translationBox')
+        const p = document.createElement('p')
+        p.className = styles.translatedText
+        p.textContent = translate
+        div.appendChild(p)
+        div.scrollTo(0, div.scrollHeight)
+        // const synth = window.speechSynthesis
+        // const utterance = new SpeechSynthesisUtterance(translate)
+        // synth.speak(utterance)
+    }
+
+    addTranslate()
+  }, [translate])
+
+//   document.getElementById('input').addEventListener('change', () => {
+//     if (audio == false) {
+//     setAudio(true)
+//     } else {
+//         setAudio(false)
+//     }
+//   })
 
     return (
         <div className={styles.translatePage}>
             <h1>Debabel</h1>
             <div className={styles.outer}>
-            <div className={styles.translationBox}>
+            <div id='translationBox' className={styles.translationBox}>
+                {/* <p className={styles.translatedText}>ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss</p>
                 <p className={styles.translatedText}>ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss</p>
                 <p className={styles.translatedText}>ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss</p>
                 <p className={styles.translatedText}>ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss</p>
@@ -44,8 +82,7 @@ const Translate = () => {
                 <p className={styles.translatedText}>ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss</p>
                 <p className={styles.translatedText}>ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss</p>
                 <p className={styles.translatedText}>ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss</p>
-                <p className={styles.translatedText}>ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss</p>
-                <p className={styles.translatedText}>ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss</p>
+                <p className={styles.translatedText}>ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss</p> */}
             </div>
             </div>
             <div>
