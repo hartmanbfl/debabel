e7a84b9054eccb1c0a5d128d3e5f830fb3c77074
Ben Hartman <ben.hartman@unblu.com>
2023-11-13 17:15:11 +0100

    Initial code

--- src/LogoComponent.js
+++ src/LogoComponent.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 const LogoComponent = (props) => {
+    const [data, setData] = useState([]);
 
     // Get the Server name from environment variable
     const serverName = process.env.NEXT_PUBLIC_SERVER_NAME;
-    const churchName = props.churchName;
+
+    useEffect(() => {
+        fetch('https://example.com/api/data')
+          .then(response => response.json())
+          .then(data => setData(data));
+      }, []);
+
+
     const url = `${serverName}/img/${churchName}.png`
 
     return <div> <img src={url} alt="Church Logo"></img></div>;
