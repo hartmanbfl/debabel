18af508f9dc75156b00e7af58724b739fdc29a9f
AdieDickKC <56456682+AdieDickKC@users.noreply.github.com>
2024-02-07 22:37:20 +0000

    Use correct action name

--- src/DisplayTranscriptButtonComponent.js
+++ src/DisplayTranscriptButtonComponent.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import styles from '@/styles/DisplayTranscript.module.css'
 
-const DisplayTranscriptButtonComponent = ({ onClick }) => {
+const DisplayTranscriptButtonComponent = ({ onChange }) => {
     
     return (
         <>
