a59e0378de33ba42a6012cfc57226b0f81ced10e
AdieDickKC <56456682+AdieDickKC@users.noreply.github.com>
2024-02-07 22:34:14 +0000

    Shift logic for display transcript button in to index.js

--- src/DisplayTranscriptButtonComponent.js
+++ src/DisplayTranscriptButtonComponent.js
@@ -1,22 +1,13 @@
 import React, { useEffect, useState } from "react";
 import styles from '@/styles/DisplayTranscript.module.css'
 
-const DisplayTranscriptButtonComponent = ({ diplayTranscript }) => {
-    const [displayTranscript, setDisplayTranscript] = useState(false)
-
-    const handleDisplayTranscriptChange = () => {
-        setDisplayTranscript(!displayTranscript)
-    }
-
-    useEffect(() => {
-        console.log(`Display Transcript is now: ${displayTranscript}`);
-    }, [displayTranscript]);
-
+const DisplayTranscriptButtonComponent = ({ onClick }) => {
+    
     return (
         <>
             <div className={styles.transcriptButton}>
                 <label className={styles.switch}>
-                    <input id='input' type="checkbox" onChange={handleDisplayTranscriptChange} />
+                    <input id='input' type="checkbox" onChange={onChange} />
                     <span className={styles.slider}></span>
                 </label>
                 <p>Transcript</p>
